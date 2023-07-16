0868a05315190b14bda7fc13a05118416f2447e8
Michel França <michel.assis@targetsoftware.com.br>
2023-07-16 06:47:00 -0300

    feat: trabalhando com volumes em containers

---
+++ section-02/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mensagens</title>
+</head>
+
+<body>
+  <h1>Escreva sua Mensagem EM TEMPO REAL</h1>
+
+  <form action="process.php" method="POST">
+    <input type="text" name="message" id="message">
+    <button type="submit">Enviar Mensagem</button>
+  </form>
+</body>
+
+</html>
